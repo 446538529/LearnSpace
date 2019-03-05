e03a351b9c860a203bc803c36535b7bbcbec02d6
yang.ting <zhuweng.yt@dtyunxi.com>
2019-03-05 21:14:53 +0800

    zuul

--- server-1/src/main/java/com/learn/server1/feign/TestApi.java
+++ server-1/src/main/java/com/learn/server1/feign/TestApi.java
@@ -3,6 +3,9 @@ package com.learn.server1.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * FeignClient 自带负载均衡
+ */
 @FeignClient(value = "client-1")
 public interface TestApi {
     @GetMapping("/port")
